e435bd44715ea63ba488ebac24094337fead8017
David Collado Sela <bitomule@gmail.com>
2015-05-21 10:38:56 +0200

    center text

--- Source/BTButton.swift
+++ Source/BTButton.swift
@@ -41,6 +41,7 @@ public class BTButton:NSObject{
     
     internal var titlePadding:CGFloat = 5
     internal var imagesPadding:CGFloat = 5
+    internal var verticalImagesPadding:CGFloat = 5
     
     // MARK: - View constructors
     
@@ -74,7 +75,7 @@ public class BTButton:NSObject{
         container.addSubview(imageView)
         let leftConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: container, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: imagesPadding)
         let rightConstraint = NSLayoutConstraint(item: container, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: imageView, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: imagesPadding)
-        let topConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: container, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: imagesPadding)
+        let topConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: container, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: verticalImagesPadding)
         let widthConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.Width, multiplier: 1, constant: image.size.width)
         let heightConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.Height, multiplier: 1, constant: image.size.height)
         container.addConstraints([leftConstraint,rightConstraint,topConstraint,widthConstraint,heightConstraint])
@@ -88,6 +89,7 @@ public class BTButton:NSObject{
         label.text = self.title
         label.textColor = UIColor.whiteColor()
         label.numberOfLines = 0
+        label.textAlignment = NSTextAlignment.Center
         let leftConstraint = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: container, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: titlePadding)
         let rightConstraint = NSLayoutConstraint(item: container, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: label, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: titlePadding)
         let topConstraint = NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: imageView, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: titlePadding)
